3bea3618c641d60eeab60ade79b5cef77a0cf243
Wolfram Hoeps <wolfram.hoeps@embl.de>
2023-07-16 12:43:42 +0200

    Strong-Seq v0.9

--- workflow/scripts/trash/winning_script.sh
+++
@@ -1,5 +0,0 @@
-
-
-cut -f 4,6 <(bedtools intersect -a h1_ont.bam -b h1_sseq_plus_output.bam -wa -bed) > raw.bed
-cat raw.bed | uniq > raw_uniq.bed
-awk '{print $1}' raw_uniq.bed | sort | uniq -u | grep -wFf - raw_uniq.bed > tagged_reads.tsv
